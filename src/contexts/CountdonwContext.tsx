8c92188fe131bef2ab5cf4fb12ddf69034acef62
Vitória Amorim <vitoria.amorimm01@gmail.com>
2021-02-28 14:52:27 -0400

    Adding settings files

--- src/contexts/CountdonwContext.tsx
+++ src/contexts/CountdonwContext.tsx
@@ -23,7 +23,7 @@ export function CountdownProvider({children}: CountdonwProviderProps) {
     const {startNewChallenge}= useContext(ChallengeContext);
     
 
-    const [time, setTime] = useState(25*60);
+    const [time, setTime] = useState(0.1*60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
